1291edec09bffd38bc18e68f26b0714899fd17d2
samad-scs <samad_saiyed@codenticsoftware.com>
2025-06-06 14:38:59 +0530

    feat: integrate next-intl for internationalization support; add locale management and update layout for dynamic language rendering

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from 'next'
+import createNextIntlPlugin from 'next-intl/plugin'
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
@@ -15,4 +16,6 @@ const nextConfig: NextConfig = {
   }
 }
 
-export default nextConfig
+const withNextIntl = createNextIntlPlugin()
+
+export default withNextIntl(nextConfig)
--- src/utils/db/index.ts
+++ src/utils/db/index.ts
@@ -1,7 +1,7 @@
 import { prisma } from '@/prisma'
 
 export const getUserFromDb = async (email: string) => {
-  const user = await prisma.user.findUnique({ where: { email } })
+  const user = await prisma.user.findUnique({ where: { email, status: true } })
 
   return user
 }
